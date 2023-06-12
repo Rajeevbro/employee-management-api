e07d45c9f121de3968f07174ca8a29bb2341d13b
Rajeev Chapagain <rajeevchapagain01@gmail.com>
2023-06-12 14:36:59 -0500

    cross origin fixed

--- src/main/java/com/programming/rajeev/Employee/controller/EmployeeController.java
+++ src/main/java/com/programming/rajeev/Employee/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping ("/employee")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
